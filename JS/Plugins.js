5b8b13adc2c519d9a0a2e30a13b02d29ae09af98
unknown <nmg181963@hotmail.com>
2020-12-02 13:11:08 +0200

    initals

---
+++ JS/Plugins.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+	/*Start button up section */
+
+	$(".fa-chevron-circle-up").on("click", function () {
+		$("html,body").animate({ scrollTop: 0 }, 1000);
+	});
+
+	/*End button up section */
+});
